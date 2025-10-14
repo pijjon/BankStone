ad8c66952d3885095335b1a7d4b37e3ae96740b2
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-14 09:51:40 -0400

    Add home deposit ledger and makePayment methods skeleton for organization

---
+++ src/main/java/com/pluralsight/BankStone.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+public class BankStone {
+    public static void main(String[] args) {
+
+    }
+
+    public static void home() {
+
+    }
+
+    public static void deposit() {
+
+    }
+
+    public static void ledger() {
+
+    }
+
+    public static void makePayment() {
+
+    }
+}
