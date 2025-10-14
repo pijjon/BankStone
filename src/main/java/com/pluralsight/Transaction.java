1fa0be66c8fdf52d5cc7800479dd45c77ef1e575
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-14 14:52:01 -0400

    create Transaction.java class file

---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,50 @@
+package com.pluralsight;
+
+
+import java.time.LocalDateTime;
+
+public class Transaction {
+    private LocalDateTime dateTime;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public Transaction(String description, String vendor, double amount) {
+        this.dateTime = LocalDateTime.now();
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
