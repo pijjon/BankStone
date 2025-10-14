40d17765d0aff1623be4aa6f77aca250e49fb4c2
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-14 10:57:09 -0400

    import and create static scanner and askUser method for handling user input prompting

--- src/main/java/com/pluralsight/BankStone.java
+++ src/main/java/com/pluralsight/BankStone.java
@@ -1,8 +1,22 @@
 package com.pluralsight;
 
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class BankStone {
+    public static Scanner myScanner = new Scanner(System.in);
     public static void main(String[] args) {
 
+    }
+
+    public static String askUser(String question) {
+        try {
+            return myScanner.nextLine();
+        }
+        catch (Exception e) { // usually only catches if there is something wrong with the scanner
+            System.out.println("Error prompting for user input");
+            return "";
+        }
     }
 
     public static void home() {
