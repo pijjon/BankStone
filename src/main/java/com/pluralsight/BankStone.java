cb4ea8064f29aa1a1869aadece96fd718d696458
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-14 15:32:23 -0400

    create askUserDouble() method for prompting user for double value

--- src/main/java/com/pluralsight/BankStone.java
+++ src/main/java/com/pluralsight/BankStone.java
@@ -1,9 +1,13 @@
 package com.pluralsight;
 
+import java.time.LocalDateTime;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class BankStone {
     public static Scanner myScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         home(); // start application and display home screen
     }
@@ -11,10 +15,24 @@ public class BankStone {
     public static String askUser(String question) {
         try {
             return myScanner.nextLine();
-        }
-        catch (Exception e) { // usually only catches if there is something wrong with the scanner
+        } catch (Exception e) { // usually only catches if there is something wrong with the scanner
             System.out.println("Error prompting for user input");
             return "";
+        }
+    }
+
+    public static double askUserDouble(String question) {
+        while (true) { // keep looping indefinitely
+            try {
+                double response = myScanner.nextDouble();
+                myScanner.nextLine(); // eat the line
+                return response; // until a return statement is reached (return breaks the while loop)
+            } catch (InputMismatchException e) {
+                System.out.println("Incorrect input type. Try again!");
+            } catch (NoSuchElementException | IllegalStateException e) {
+                e.printStackTrace();
+                System.out.println("Something went wrong. Try again!");
+            }
         }
     }
 
@@ -38,15 +56,14 @@ public class BankStone {
             if (response == null || response.isEmpty()) {
                 System.out.println("Response not valid. Please try again");
                 continue;
-            }
-            else {
+            } else {
                 switch (response.toLowerCase()) {
                     case "d": {
-                        makeDeposit();
+                        makeTransaction("deposit");
                     }
 
                     case "p": {
-                        makePayment();
+                        makeTransaction("charge");
                     }
 
                     case "l": {
@@ -62,12 +79,13 @@ public class BankStone {
             }
 
         }
-
-
     }
 
-    public static void makeDeposit() {
+    public static void makeTransaction(String type) {
+        if (type.equals("deposit")) {
+            double amount = askUserDouble("How much money would you like to deposit?");
 
+        }
     }
 
     public static void viewLedger() {
@@ -75,6 +93,10 @@ public class BankStone {
     }
 
     public static void makePayment() {
+
+    }
+
+    public static void preload() {
 
     }
 }
